ed4aba36e583ef603b14f3f0afd9575b2e7b0d06
Fly Xu <fly.xu@lcola.cn>
2021-01-07 09:52:02 +0800

    test

---
+++ day03/presuffix.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "this is an example"
+
+	fmt.Println(str)
+	
+	// 是否是以 this 为前缀
+	fmt.Println(strings.HasPrefix(str, "this"))
+	// 是否以 examples 为后缀
+	fmt.Println(strings.HasSuffix(str, "examples"))
+	// 获取字符串 "i" 在字符串中第一次出现的索引，-1 表示没有找到(不包含该字符串)
+	fmt.Println(strings.Index(str, "i"))
+	// 获取字符串 'i" 在字符串中最后一次出现的索引，-1 表示没有找到
+	fmt.Println(strings.LastIndex(str, "i"))
+}
